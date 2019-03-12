a0505f2186ef64d1a2efff411db1790b8207985a
Rabbit <Corbineau@users.noreply.github.com>
2019-03-12 00:09:06 -0400

    WIP: index html, beginnings of js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,18 @@
 
+var progressWord = []
+
 
 
 var game = {
+    words: ["Charlie", "Alpha", "Tomcat"],
+
+    
+    newWord: function() {
+        for (var i = 0; i < wordLength; i ++)
+            progressWord.push('-');
+
+    }
+
 
 
 }
